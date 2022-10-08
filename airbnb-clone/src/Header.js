b64de38b9bef083e3e131bd06882ff18957b1980
Swapnodeep Biswas <bonny2610@gmail.com>
2022-10-08 15:03:00 +0530

    completed header structure

--- airbnb-clone/src/Header.js
+++ airbnb-clone/src/Header.js
@@ -1,5 +1,9 @@
 import React from "react";
 import "./Header.css";
+import LanguageIcon from "@material-ui/icons/Language";
+import SearchIcon from "@material-ui/icons/Search";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import { Avatar } from "@material-ui/core";
 
 function Header() {
   return (
@@ -10,11 +14,18 @@ function Header() {
       {/* Search Bar */}
       <div className="header__center">
         <input type="text" />
-        
+
         {/* We are using Material UI to get the search icon */}
         {/* npm install @material-ui/core */}
         {/* npm install @material-ui/icons */}
         <SearchIcon />
+      </div>
+
+      <div className="header__right">
+        <p>Become a host</p>
+        <LanguageIcon/>
+        <ExpandMoreIcon />
+        <Avatar />
       </div>
     </div>
   );
